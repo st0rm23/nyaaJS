58c418a8fd07670375d44ff935e82f7cdaceab5a
xingduan <xingduan950211@gmail.com>
2015-11-18 15:31:13 +0800

    add nyaa.request.js

---
+++ src/nyaa.request.js
@@ -0,0 +1,45 @@
+$nyaa.register(
+	'request',
+	[],
+	{
+		onLoad: function(){
+			if(typeof(jQuery) === 'undefined'){
+				this.nyaa.error('nyaa.request requires jQuery');
+				return false;
+			}
+			this.nyaa.debug('request onLoad');
+			console.log(this);
+			$nyaa.ready(function(){
+				window.onhashchange = function(){
+					$nyaa.request._hash = false;
+					$nyaa.event('HashChange');
+				}
+				window.onhashchange();
+			});
+		},
+		_query: false,
+		query: function(key){
+			if(this._query === false){
+				this._query = {};
+				var re = /(?:([^&=\?]+?)(?:=([^&=\?]*?))?)(?:&|$)/ig;
+				var result;
+				while(result = re.exec(location.search)){
+					this._query[result[1]] = result[2] === undefined?'':result[2];
+				}
+			}
+			return this._query[key] === undefined?null:this._query[key];
+		},
+		_hash: false,
+		hash: function(key){
+			if(this._hash === false){
+				this._hash = {};
+				var re = /(?:([^&=#]+?)(?:=([^&=#]*?))?)(?:&|$)/ig;
+				var result;
+				while(result = re.exec(location.hash)){
+					this._hash[result[1]] = result[2] === undefined?'':result[2];
+				}
+			}
+			return this._hash[key] === undefined?null:this._hash[key];
+		}
+	}
+);
