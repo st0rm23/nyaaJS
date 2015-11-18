4e76a72fffdaa1f1acbee46d873285a73184952a
xingduan <xingduan950211@gmail.com>
2015-11-18 14:55:28 +0800

    add nyaa.ajax.js

---
+++ src/nyaa.ajax.js
@@ -0,0 +1,55 @@
+$nyaa.register(
+	'ajax',
+	[],
+	{
+		onLoad: function(){
+			if(typeof(jQuery) === 'undefined'){
+				this.nyaa.error('nyaa.ajax requires jQuery');
+				return false;
+			}
+			this.nyaa.debug('ajax onLoad');
+			console.log(this);
+		},
+		_ajax: function(method, url, data, success, error){
+			$.ajax({
+				crossDomain: true,
+				cache: false,
+				xhrFields: {
+					withCredentials: true
+				},
+				type: method,
+				url: url,
+				data: data,
+				success: success,
+				error: error,
+				dataType: 'json'
+			});
+		},
+		get: function(url, data, success, error){
+			if(typeof(data) === 'undefined') data = {};
+			if(typeof(data) === 'function'){
+				error = success;
+				success = data;
+				data = {};
+			}
+			data[$('meta[name=csrf-param]').attr('content')] = $('meta[name=csrf-token]').attr('content');
+			this._ajax('POST', url, data, success, error);
+		},
+		post: function(url, data, success, error){
+			if(typeof(data) === 'undefined') data = {};
+			if(typeof(data) === 'function'){
+				error = success;
+				success = data;
+				data = {};
+			}
+			data[$('meta[name=csrf-param]').attr('content')] = $('meta[name=csrf-token]').attr('content');
+			var query = '';
+			for(var key in data){
+				if(query !== '') query += '&';
+				query += encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+			}
+			if(query !== '') url += '?' + query;
+			this._ajax('GET', url, {}, success, error);
+		}
+	}
+);
